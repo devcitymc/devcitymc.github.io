2e209605675d209c1b685b4b6c0ffdab386c0fb1
TechnoDrive <zarifabrar02@gmail.com>
2021-01-03 12:31:45 -0500

    Add Nether Bridges R' Us site

---
+++ shops/nether-bridges-r-us/js/main.js
@@ -0,0 +1,33 @@
+let formula = (safety, efficency, style, distance, peril) => {
+        return ((safety + efficency + style) / 3) * distance * peril;
+    },
+    inRange = (n, min, max) => {
+        return x >= min && x <= max;
+    },
+    ranges = {
+        A: 100,
+        B: 89,
+        C: 79,
+        D: 69,
+        F: 59
+    },
+    prices = {
+        A: 10,
+        B: 25,
+        C: 50,
+        D: 75,
+        F: 100
+    }
+
+document.forms['options'].onsubmit = event => {
+    let safety = document.getElementById('safety'),
+        efficiency = document.getElementById('efficiency'),
+        style = document.getElementById('style'),
+        distance = document.getElementById('distance'),
+        peril = document.querySelectorAll('#peril option:checked');
+
+    let perilValue = Array.from(peril).map(el => el.value).length * 0.25;
+
+    console.log(safety.value, efficiency.value, style.value, distance.value, perilValue);
+    event.preventDefault();
+}
